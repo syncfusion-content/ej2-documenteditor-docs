3a76fc3ce715662e686a9f10313d3693eda1f720
BalamuruganShanmugam <balamurugan.shanmugam@syncfusion.com>
2023-12-26 16:15:11 +0530

    commit changes

--- ej2-javascript/code-snippet/document-editor/open-aws-s3/index.ts
+++ ej2-javascript/code-snippet/document-editor/open-aws-s3/index.ts
@@ -10,25 +10,31 @@ let documentEditorContainer: DocumentEditorContainer = new DocumentEditorContain
 // Render Document Editor component.
 documentEditorContainer.appendTo('#DocumentEditor');
 
-fetch(
-  'http://localhost:62870/api/documenteditor/LoadFromS3',
-  {
+document.getElementById('load').addEventListener('click', (): void => {
+  load();
+});
+
+function load(): void {
+  fetch(
+    'http://localhost:62870/api/documenteditor/LoadFromS3',
+    {
       method: 'Post',
       headers: { 'Content-Type': 'application/json;charset=UTF-8' },
       body: JSON.stringify({ documentName: 'Getting Started.docx' })
-  }
-)
-  .then(response => {
+    }
+  )
+    .then(response => {
       if (response.status === 200 || response.status === 304) {
-          return response.json(); // Return the Promise
+        return response.json(); // Return the Promise
       } else {
-          throw new Error('Error loading data');
+        throw new Error('Error loading data');
       }
-  })
-  .then(json => {
-    documentEditorContainer.documentEditor.open(JSON.stringify(json));
-  })
-  .catch(error => {
+    })
+    .then(json => {
+      documentEditorContainer.documentEditor.open(JSON.stringify(json));
+    })
+    .catch(error => {
       console.error(error);
-  });
+    });
+}
 
